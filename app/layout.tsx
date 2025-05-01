84b519ce412c074d321f3a797525144f6b30edb9
sahil-empiric <sahil.kp@empiricinfotech.com>
2025-05-01 12:18:30 +0530

    refactor: simplify layout component by removing unnecessary fragment

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning>
-        <>
-          {children}
-        </>
+        {children}
       </body>
     </html>
   )
